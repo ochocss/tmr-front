96c082037d2a3c1baff06958f29bd6819526bc10
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-03-10 10:00:01 -0300

    Upload files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import './App.css';
+import RequestsService from "./services/requestsService";
 import React, { useEffect, useState } from "react";
 
+const ENDPOINT = "";
+
 function App() {
-  const [message, setMessage] = useState("");
+  const [tasks, setTasks] = useState(null);
 
-    useEffect(() => {
-        fetch("http://localhost:8080")
-            .then(response => response.text())
-            .then(data => setMessage(data))
-            .catch(error => console.error("Erro:", error));
-    }, []);
+  useEffect(() => {
+    async function fetchMessage() {
+      try {
+        setTasks(await new RequestsService(ENDPOINT).get());
+      } catch (err) {
+        console.error(err);
+      }
+    }
 
-    console.log(message);
-    return <div><h1>{message}</h1></div>;
+    fetchMessage();
+  }, []);
+  
+  return (
+    <div>
+      <h1>Task List</h1>
+      {tasks ? (tasks.map(element => (
+        <>
+          <div key={element.id} style={{border:"3px solid black"}}>
+            <p><strong>Type: </strong> {element.typeId}</p>
+            <p><strong>Subject: </strong> {element.subjectId}</p>
+            <p><strong>Description: </strong> {element.description}</p>
+            <p><strong>Date: </strong> {element.date}</p>
+          </div>
+          <br />
+        </>
+      ))) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/services/requestsService.js
@@ -0,0 +1,38 @@
+const URL = "http://localhost:8080"
+
+class RequestsService {
+    constructor(endpoint) {
+        this.endpoint = endpoint
+    }
+
+    async get() {
+        let result;
+        await fetch(URL + this.endpoint, {method: "GET"})
+            .then(response => response.json())
+            .then(data => result = data)
+            .catch(err => console.error(err));
+        return result;
+    }
+
+    async post() {
+        fetch(URL + this.endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "aplication/json"
+            },
+            body: JSON.stringify({
+                name: "a"
+            })
+        })
+    }
+
+    async update() {
+
+    }
+
+    async delete() {
+
+    }
+}
+
+export default RequestsService;
