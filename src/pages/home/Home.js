1e84c2e6ef765d0b6095cde409067d6327a68cf2
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-04-11 11:00:40 -0300

    Change home components

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,12 @@
+import 'primeicons/primeicons.css';
 import './Home.css';
 import RequestsService from "../../services/requestsService";
 import React, { useEffect, useState, useId } from "react";
 import { taskTypes } from '../../model/taskTypes';
+import { Button } from 'primereact/button';
+import { Link } from 'react-router-dom';
 
-const ENDPOINT = "/";
+const ENDPOINT = "";
 
 function Home() {
   const [tasks, setTasks] = useState(null);
@@ -22,36 +25,46 @@ function Home() {
   }, []);
   
   return (
-    <div>
-      <h1>Task List</h1>
-      {tasks ? (tasks.length === 0 ? (
-        <p>No tasks created.</p>
-      ) : (tasks.map(element => (
+    <>
+      <div className="centralize">
+        <h1>Task List</h1>
+      </div>
+      {tasks ? (
         <>
-          <div key={element.id} id={element.id} style={{border:"3px solid black"}}>
-            <div className="lefttaskcontainer">
-              <p><strong>Type: </strong> {element.typeId}</p>
-              <p><strong>Subject: </strong> {taskTypes.get(element.subjectId)}</p>
-              <p><strong>Description: </strong> {element.description}</p>
-              <p><strong>Date: </strong> {element.date}</p>
-              <p><strong>Days left: </strong> {Math.abs(new Date() - new Date(element.date)) / (1000 * 60 * 60 * 24)}</p>
+          <Link to="/create"><Button className="create-button" label="Create task" icon="pi pi-plus"
+              onClick={() => {
+              
+            }} /></Link>
+          {(tasks.length === 0 ? (
+            <p>No tasks created.</p>
+          ) : (tasks.map(element => (
+            <div className="task" key={element.id} id={element.id}>
+              <div>
+                <p><strong>Type: </strong> {element.typeId}</p>
+                <p><strong>Subject: </strong> {taskTypes.get(element.subjectId)}</p>
+                <p><strong>Description: </strong> {element.description}</p>
+                <p><strong>Date: </strong> {element.date}</p>
+                <p><strong>Days left: </strong> {Math.abs(new Date() - new Date(element.date)) / (1000 * 60 * 60 * 24)}</p>
+              </div>
+              <div className="buttons">
+                <Link to="/edit"><Button className="edit-button" type="button" icon="pi pi-file-edit" label="Edit"
+                  onClick={() => {
+                  
+                }} /></Link>
+                <Button className="delete-button" type="button" icon="pi pi-trash" label="Delete"
+                  onClick={async () => {
+                  await requestService.delete(element.id);
+                  document.getElementById(element.id).remove();
+                }} />
+              </div>
             </div>
-            <div className="righttaskcontainer">
-              <button className="deletebutton" type="button" onClick={async () => {
-                await requestService.delete(element.id);
-                document.getElementById(element.id).remove();
-              }}>Delete</button>
-              <button className="editbutton" type="button" onClick={() => {
-                
-              }}>Edit</button>
-            </div>
-          </div>
-          <br />
+          )))
+          )}
         </>
-      )))) : (
+      ) : (
         <p>Loading...</p>
       )}
-    </div>
+    </>
   );
 }
 
