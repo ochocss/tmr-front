45f8b9c131031a933ce733994608bab75841f819
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-04-11 11:00:51 -0300

    Remove unused files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import 'primereact/resources/themes/md-dark-deeppurple/theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './Layout'
 import Home from './pages/home/Home'
 import TaskEditor from './pages/taskeditor/TaskEditor'
 
@@ -14,6 +14,11 @@ root.render(
         <Route path="/create" element={<TaskEditor />} />
       </Routes>
     </BrowserRouter>
-    <Layout />
+    <div style={{display: "flex", justifyContent: "center", width: "100%", position: "relative", bottom: "0"}}>
+      <p style={{opacity: "0.8"}}>
+        Made by  
+        <span style={{color: "rgb(112, 131, 233)", cursor: "pointer"}} onClick={() => window.location.href = "https://github.com/ochocss"}> chocs </span>
+      </p>
+    </div>
   </>
 );
