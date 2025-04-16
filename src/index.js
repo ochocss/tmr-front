3f9c0bbe207941a7861821aa91932a0abaa053d2
Fernando Hirsch <fernandobhirsch@gmail.com>
2025-04-16 01:12:43 -0300

    Reorganize files

--- src/index.js
+++ src/index.js
@@ -2,24 +2,11 @@ import './index.css';
 import 'primereact/resources/themes/md-dark-deeppurple/theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home'
-import TaskEditor from './pages/taskeditor/TaskEditor'
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<TaskEditor />} />
-      </Routes>
-    </BrowserRouter>
-    <div style={{display: "flex", justifyContent: "center", width: "100%", position: "relative", bottom: "0"}}>
-      <p style={{opacity: "0.8"}}>
-        Made by  
-        <span style={{color: "rgb(112, 131, 233)", cursor: "pointer"}} onClick={() => window.location.href = "https://github.com/ochocss"}> chocs </span>
-      </p>
-    </div>
+    <App />
   </>
 );
